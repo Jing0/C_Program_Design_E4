eb12e6f63eb4ad27f15e22053a407299ab78ae12
Jing0 <j.kuo2012@gmail.com>
2014-03-27 01:01:32 +0800

    optimized

--- 9-3.c
+++ 9-3.c
@@ -1,26 +1,10 @@
 #include <stdio.h>
-#define SIZE 5
+#define SIZE 2
 struct Student{
 	int num;
 	char name[10];
 	int score[3];
 }stud[SIZE];
-int main(){
-	int i,j;
-	struct Student *p;
-	void print(struct Student *p);
-	for(i=0;i<SIZE;i++){
-		printf("Please input num and name:");
-		scanf("%d%s",&stud[i].num,stud[i].name);
-		for(j=0;j<3;j++){
-			printf("Please input score of Sub%d:",j+1);
-			scanf("%d",&stud[i].score[j]);
-		}
-	}
-	p=stud;
-	print(p);
-	return 0;
-}
 void print(struct Student *p){
 	int i,j;
 	for(i=0;i<SIZE;i++){
@@ -29,4 +13,19 @@ void print(struct Student *p){
 			printf("Sub%d:%d\t",i+1,stud[i].score[1]);
 		putchar('\n');
 	}
+}
+int main(){
+	int i,j;
+	struct Student *p;
+	p=stud;
+	for(i=0;i<SIZE;i++){
+		printf("Please input num and name:");
+		scanf("%d%s",&stud[i].num,stud[i].name);
+		for(j=0;j<3;j++){
+			printf("Please input score of Sub%d:",j+1);
+			scanf("%d",&stud[i].score[j]);
+		}
+	}
+	print(p);
+	return 0;
 }
