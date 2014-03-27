a6a03329a94aa3dac82b92cfcbf348e5c76772f3
Jing0 <j.kuo2012@gmail.com>
2014-03-28 00:07:19 +0800

    7-9_1

--- 7-9.c
+++ 7-9.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
-int letter=0,num=0,space=0,other=0;
 int main(){
 	char str[100];
 	void count(char str[]);
 	printf("Please input a character string:\n");
 	gets(str);
-	//scanf("%s",str);
 	count(str);
-	printf("Letters:%d\nNumbers:%d\nSpaces:%d\nOthers:%d\n",letter,num,space,other);
 	return 0;
 }
 void count(char str[]){
-	int i;
-	for(i=0;str[i]!='\0';i++){
+	int i,letter,num,space,other;
+	for(i=letter=num=space=other=0;str[i]!='\0';i++)
 		if(str[i]>='0'&&str[i]<='9')
-			num=num+1;
+			num+=1;
 		else if((str[i]>='A'&&str[i]<='Z')||(str[i]>='a'&&str[i]<='z'))
-			letter=letter+1;
+			letter+=1;
 		else if(str[i]==32)
-			space=space+1;
+			space+=1;
 		else
-			other=other+1;
-	}
+			other+=1;
+	printf("Letters:%d\nNumbers:%d\nSpaces:%d\nOthers:%d\n",letter,num,space,other);
 }
