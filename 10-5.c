aa12829b5ef1b4fc012eaa5ca375ca444284ddd4
Jing0 <j.kuo2012@gmail.com>
2014-03-28 23:22:18 +0800

    10-5

---
+++ 10-5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#define SIZE 5
+struct Student{
+	int num;
+	char name[10];
+	int sub[3];
+	float average;
+}stud[SIZE];
+void save(){
+	FILE *fp;
+	int i;
+	if((fp=fopen("stud","w"))==NULL){
+		printf("cannot open file\n");
+		return;
+	}
+	for(i=0;i<SIZE;i++)
+		if(fwrite(&stud[i],sizeof(struct Student),1,fp)!=1){
+			if(feof(fp)){
+				fclose(fp);
+				return;
+			}
+			printf("file read error\n");
+		}
+	fclose(fp);
+}
+int main(){
+	int i,j;
+	float sum[3]={0};
+	for(i=0;i<SIZE;i++){
+		printf("Please input ID and name of student NO.%d:",i+1);
+		scanf("%d%s",&stud[i].num,stud[i].name);
+		for(j=0;j<3;j++){
+			printf("Please input the score of subject NO.%d:",j+1);
+			scanf("%d",&stud[i].sub[j]);
+			sum[i]+=stud[i].sub[j];
+		}
+		stud[i].average=sum[i]/3;
+	}
+	save();
+	return 0;
+}
